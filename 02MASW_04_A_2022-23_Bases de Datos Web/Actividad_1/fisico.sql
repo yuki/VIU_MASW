5f44baf9ef952daeff40c392d5fb46ed8a06c1bf
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2022-05-19 20:10:05 +0200

    Añadiendo cosas a Actividad 1 de base de datos
    Movido el contenido del word al notebook

--- 02MASW_04_A_2022-23_Bases de Datos Web/Actividad_1/fisico.sql
+++ 02MASW_04_A_2022-23_Bases de Datos Web/Actividad_1/fisico.sql
@@ -1,126 +1,128 @@
 
-CREATE DOMAIN genero_deportista as varchar CHECK(
+CREATE DOMAIN genero_deportista as VARCHAR CHECK(
     VALUE IN ('masculino','femenino','nd')
 );
 
 
 
 CREATE TABLE DEPORTISTA(
-    deportista_id serial not null primary key,
-    nombre varchar(20) not null,
-    apellidos varchar(30),
+    deportista_id SERIAL not null primary key,
+    nombre VARCHAR(20) not null,
+    apellidos VARCHAR(30),
     fecha_nacimiento date,
-    genero genero_deportista
+    genero genero_deportista,
+    usuario VARCHAR (20) not null,
+    password VARCHAR (20) not null
 );
 
 
 CREATE TABLE FEDERACION(
-    federacion_id serial not null primary key,
-    nombre varchar(50),
-    direccion varchar(70),
-    web varchar(50)
+    federacion_id SERIAL not null primary key,
+    nombre VARCHAR(50),
+    direccion VARCHAR(70),
+    web VARCHAR(50)
 );
 
 CREATE TABLE DEPORTE(
-    deporte_id serial not null primary key,
-    nombre varchar(50),
-    federacion_id integer not null REFERENCES federacion(federacion_id) ON DELETE CASCADE ON UPDATE CASCADE
+    deporte_id SERIAL not null primary key,
+    nombre VARCHAR(50),
+    federacion_id INTEGER not null REFERENCES federacion(federacion_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE ENTRENAMIENTO(
-    entrenamiento_id serial not null primary key,
-    nombre varchar(50),
-    fecha timestamptz,
-    duracion integer,
-    distancia integer,
-    deportista_id integer not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    deporte_id integer REFERENCES deporte(deporte_id) ON DELETE SET NULL ON UPDATE CASCADE
+    entrenamiento_id SERIAL not null primary key,
+    nombre VARCHAR(50),
+    fecha TIMESTAMPTZ,
+    duracion INTEGER,
+    distancia INTEGER,
+    deportista_id INTEGER not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    deporte_id INTEGER not null REFERENCES deporte(deporte_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
 CREATE TABLE CAMPEONATO(
-    campeonato_id serial not null primary key,
-	nombre varchar(50),
-	fecha_inicio timestamptz,
-	fecha_fin timestamptz,
-	federacion_id integer not null REFERENCES federacion(federacion_id) ON DELETE CASCADE ON UPDATE CASCADE
+    campeonato_id SERIAL not null primary key,
+    nombre VARCHAR(50),
+    fecha_inicio TIMESTAMPTZ,
+    fecha_fin TIMESTAMPTZ,
+    federacion_id INTEGER not null REFERENCES federacion(federacion_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 CREATE TABLE PRUEBA(
-    prueba_id serial not null primary key,
-	nombre varchar(50),
-	distancia integer,
-	deporte_id integer REFERENCES deporte(deporte_id) ON DELETE SET NULL ON UPDATE CASCADE
+    prueba_id SERIAL not null primary key,
+    nombre VARCHAR(50),
+    distancia INTEGER,
+    deporte_id INTEGER not null REFERENCES deporte(deporte_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
 CREATE TABLE LOGRO(
-    logro_id serial not null primary key,
-	nombre varchar(50)
+    logro_id SERIAL not null primary key,
+    nombre VARCHAR(50)
 );
 
 
 CREATE TABLE RETO(
-    reto_id serial not null primary key,
-	nombre varchar(50),
-	descripcion varchar(200),
-	fecha_inicio timestamptz,
-	fecha_fin timestamptz,
-	distancia integer,
-	tiempo integer,
-	deporte_id integer REFERENCES deporte(deporte_id) ON DELETE SET NULL ON UPDATE CASCADE,
-	logro_id integer REFERENCES logro(logro_id) ON DELETE SET NULL ON UPDATE CASCADE
+    reto_id SERIAL not null primary key,
+    nombre VARCHAR(50),
+    descripcion VARCHAR(200),
+    fecha_inicio TIMESTAMPTZ,
+    fecha_fin TIMESTAMPTZ,
+    distancia INTEGER,
+    tiempo INTEGER,
+    deporte_id INTEGER not null REFERENCES deporte(deporte_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    logro_id INTEGER not null REFERENCES logro(logro_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
 CREATE TABLE EVENTO(
-    evento_id serial not null primary key,
-	nombre varchar(50),
-	descripcion varchar(200),
-	fecha_inicio timestamptz,
-	distancia integer,
-	federacion_id integer not null REFERENCES federacion(federacion_id) ON DELETE CASCADE ON UPDATE CASCADE
+    evento_id SERIAL not null primary key,
+    nombre VARCHAR(50),
+    fecha_inicio TIMESTAMPTZ,
+    distancia INTEGER,
+    federacion_id INTEGER not null REFERENCES federacion(federacion_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 CREATE TABLE PRACTICA(
-    deportista_id integer not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	deporte_id integer REFERENCES deporte(deporte_id) ON DELETE SET NULL ON UPDATE CASCADE,
-	federado boolean,
-	PRIMARY KEY (deportista_id,deporte_id)
+    deportista_id INTEGER not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    deporte_id INTEGER not null REFERENCES deporte(deporte_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    federado boolean,
+    numero_federado INTEGER,
+    PRIMARY KEY (deportista_id,deporte_id)
 );
 
 
 CREATE TABLE ASISTE(
-    deportista_id integer not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	evento_id integer REFERENCES evento(evento_id) ON DELETE SET NULL ON UPDATE CASCADE,
-	tiempo_realizado integer,
-	PRIMARY KEY (deportista_id,evento_id)
+    deportista_id INTEGER not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    evento_id INTEGER not null REFERENCES evento(evento_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    tiempo_realizado INTEGER,
+    PRIMARY KEY (deportista_id,evento_id)
 );
 
 
 CREATE TABLE COMPITE(
-    deportista_id integer not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	prueba_id integer REFERENCES prueba(prueba_id) ON DELETE SET NULL ON UPDATE CASCADE,
-	campeonato_id integer REFERENCES campeonato(campeonato_id) ON DELETE SET NULL ON UPDATE CASCADE,
-	tiempo_realizado integer,
-	PRIMARY KEY (deportista_id,prueba_id,campeonato_id)
+    deportista_id INTEGER not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    prueba_id INTEGER not null REFERENCES prueba(prueba_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    campeonato_id INTEGER not null REFERENCES campeonato(campeonato_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    tiempo_realizado INTEGER,
+    PRIMARY KEY (deportista_id,prueba_id,campeonato_id)
 );
 
 
 CREATE TABLE HACE(
-    deportista_id integer not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	reto_id integer REFERENCES reto(reto_id) ON DELETE SET NULL ON UPDATE CASCADE,
-	fecha_unirse timestamptz,
-	fecha_completado timestamptz,
-	PRIMARY KEY (deportista_id,reto_id)
+    deportista_id INTEGER not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    reto_id INTEGER not null REFERENCES reto(reto_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    fecha_unirse TIMESTAMPTZ,
+    fecha_completado TIMESTAMPTZ,
+    PRIMARY KEY (deportista_id,reto_id)
 );
 
 
 CREATE TABLE OBTIENE(
-    deportista_id integer not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	logro_id integer REFERENCES logro(logro_id) ON DELETE SET NULL ON UPDATE CASCADE,
-	fecha timestamptz,
-	PRIMARY KEY (deportista_id,logro_id)
+    deportista_id INTEGER not null REFERENCES deportista(deportista_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    logro_id INTEGER not null REFERENCES logro(logro_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    fecha TIMESTAMPTZ,
+    PRIMARY KEY (deportista_id,logro_id)
 );
