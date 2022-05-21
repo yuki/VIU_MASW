78b4eb1e03ac3736661602d3654ba6561e34cf17
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2022-05-21 20:33:18 +0200

    pequeñas modificaciones

---
+++ 02MASW_04_A_2022-23_Bases de Datos Web/Actividad_1/consultas.sql
@@ -0,0 +1,24 @@
+SELECT d.nombre, l.nombre as logro, o.fecha 
+  FROM deportista d 
+    LEFT JOIN obtiene o ON d.deportista_id = o.deportista_id 
+    LEFT JOIN logro l ON o.logro_id = l.logro_id 
+  WHERE l.logro_id  = 1 
+  ORDER BY fecha;
+
+
+SELECT d.nombre, d.apellidos, ca.nombre AS campeonato, p.nombre AS prueba, c.tiempo_realizado 
+  FROM deportista d 
+    LEFT JOIN compite c ON d.deportista_id = c.deportista_id 
+    LEFT JOIN campeonato ca ON c.campeonato_id = ca.campeonato_id 
+    LEFT JOIN prueba p ON c.prueba_id = p.prueba_id 
+  WHERE tiempo_realizado IS NOT NULL;
+
+
+SELECT COUNT(*) AS Logros_entregados 
+  FROM obtiene;
+
+
+SELECT d.nombre, d.apellidos, MIN(tiempo_realizado) AS record 
+  FROM compite c, deportista d 
+  WHERE prueba_id = 1 AND d.deportista_id = c.deportista_id 
+  GROUP BY (d.nombre, d.apellidos);
