4b41f954101bb31eb7ffd45a46dd242e563cdfe1
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2022-09-20 07:22:23 +0200

    Añadido de nuevo la traducción en inglés

---
+++ Actividad_laravel/src/resources/lang/es/formulario.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'name' => 'Nombre',
+    'surname' => 'Apellidos',
+    'e-mail' => 'Dirección E-Mail',
+    'dni_validation' => 'El formato del DNI tiene que ser 8 números y una letra. Ej: 12345678A',
+    'password' => 'Contraseña',
+    'password_validation' => 'La contraseña debe tener una longitud mínima de 12 y contener al menos: 3 números, 3 letras mínusculas, 3 letras mayúsculas y 3 tipos de puntuación permitidos: ¡!"·$%&\{}[]()=¿?,;.:<>-+*^|@#',
+    'confirm_password' => 'Confirme contraseña',
+    'telephone' => 'Teléfono',
+    'telefono_validation' => 'El formato del teléfono es incorrecto. Mínimo 9 y máximo 12 números. También se acepta + al inicio',
+    'iban' => 'IBAN',
+    'iban_validation' => 'Formato IBAN: AB11 2222 3333 44 1234567890 sin espacios',
+    'about' => 'Sobre ti',
+    'country' => 'País',
+    'choose_country' => 'Elige País',
+    'register' => 'Registrar',
+];
