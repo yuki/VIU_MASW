1d0836e91759166c499dcde1ef2a2d381d57e2fb
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2022-11-03 14:04:11 +0100

    Añadido Timber y nueva clase

---
+++ Actividad_1/Calculadora/app/src/main/java/com/rugoli/calculator/App.kt
@@ -0,0 +1,16 @@
+package com.rugoli.calculator
+
+import android.app.Application
+import com.github.ajalt.timberkt.d
+
+class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            timber.log.Timber.plant(timber.log.Timber.DebugTree())
+            d {"rugolid: estamos en modo debug"}
+        }
+
+    }
+}
--- Actividad_1/Calculadora/app/src/main/java/com/rugoli/calculator/MainActivity.kt
+++ Actividad_1/Calculadora/app/src/main/java/com/rugoli/calculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.rugoli.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.ajalt.timberkt.d
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        d {"rugolid: oncreate... empezamos"}
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
