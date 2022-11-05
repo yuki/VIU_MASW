e10e211b5bf73011da0c5b36fd937d7d9428ee76
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2022-11-05 10:02:42 +0100

    Funcionalidad básica hecha, ahora de verdad
    - Las operaciones matemáticas funcionan.
    - El textsize del  textview se hace más pequño.
    - Al cambiar el móvil a landscape/portrait se guarda el estado y se restaura

--- Actividad_1/Calculadora/app/src/main/java/com/rugoli/calculator/MainActivity.kt
+++ Actividad_1/Calculadora/app/src/main/java/com/rugoli/calculator/MainActivity.kt
@@ -5,11 +5,21 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.github.ajalt.timberkt.d
-import org.w3c.dom.Text
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+
+    // global variables
+    var result:Double = 0.0
+    var number:Double = 0.0
+    var operation:String = ""
+    // true if we need to clean the tresult
+    var clean:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         d { "rugolid: oncreate... empezamos" }
+        //to see actual states
+        d { "rugolid STATES: " + result.toString() + " " + number.toString() + " " + operation + " "+clean.toString()}
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -25,9 +35,10 @@ class MainActivity : AppCompatActivity() {
         val button8 = findViewById<Button>(R.id.button8)
         val button9 = findViewById<Button>(R.id.button9)
 
-        // reset and share buttons
+        // reset, share and comma buttons
         val breset = findViewById<Button>(R.id.breset)
         val bshare = findViewById<Button>(R.id.bshare)
+        val bcoma = findViewById<Button>(R.id.bcoma)
 
         // operations buttons
         val bsumar = findViewById<Button>(R.id.bsumar)
@@ -40,16 +51,11 @@ class MainActivity : AppCompatActivity() {
         // result Text View
         val tresult = findViewById<TextView>(R.id.result)
 
-        // variables
-        var result:Double = 0.0
-        var number:Double = 0.0
         val operations = arrayOf("+", "-", "*", "/")
-        var operation:String = ""
-        // true if we need to clean the tresult
-        var clean:Boolean = false
 
         // onclick actions for numbers
-        val numbers = arrayOf(button0,button1,button2,button3,button4,button5,button6,button7,button8,button9)
+        val numbers = arrayOf(button0,button1,button2,button3,button4,
+                button5,button6,button7,button8,button9)
         for ((index, element) in numbers.withIndex()) {
             element.setOnClickListener {
                 if (tresult.text.toString() == "0") {
@@ -60,6 +66,15 @@ class MainActivity : AppCompatActivity() {
                 }
                 tresult.text = tresult.text.toString() + index.toString()
                 clean = false
+            }
+        }
+
+        // coma button
+        bcoma.setOnClickListener {
+            //check if there's a coma
+            d {"check coma: " + tresult.text.toString().indexOf(',').toString()}
+            if (tresult.text.toString().indexOf(',') == -1){
+                tresult.text = tresult.text.toString() + ","
             }
         }
 
@@ -79,23 +94,51 @@ class MainActivity : AppCompatActivity() {
                 operation = operations[index]
                 clean = true
                 number = 0.0
-                result = tresult.text.toString().toDouble()
+                // sólo cogemos el texto si es 0.0, porque si no perdemos la precisión
+                if (result == 0.0){
+                    result = convert(tresult)
+                }
             }
         }
 
         // onclick action for bigual
         bigual.setOnClickListener {
             if (number == 0.0) {
-                number = tresult.text.toString().toDouble()
+                number = convert(tresult)
             }
             result = do_operation(operation,result,number)
-            if (result.toInt().toDouble() == result) {
-                tresult.text = result.toInt().toString()
-            } else {
-                tresult.text = result.toString()
-            }
+            set_tresult(result,tresult)
+            d {"rugolid: " + result.toString()}
         }
 
+    }
+
+    // we need onResume to "repaint" the textView when changed state
+    override fun onResume() {
+        super.onResume()
+        set_tresult(result,findViewById<TextView>(R.id.result))
+        d {"rugolid on RESUME"}
+        d { "rugolid STATES: " + result.toString() + " " + number.toString() + " " + operation + " "+clean.toString()}
+    }
+
+    // function to "draw" the result into TextView
+    fun set_tresult(result:Double,tresult:TextView){
+        if (check_decimals(result)) {
+            tresult.text = result.toInt().toString()
+        } else {
+            // change decimal "." into spanish ","
+            tresult.text = DecimalFormat("#.##########").format(result).replace('.',',')
+        }
+    }
+
+    // convert textview into double
+    fun convert(textView: TextView):Double{
+        return textView.text.toString().replace(',','.').toDouble()
+    }
+
+    // check if the Double has decimals
+    fun check_decimals(number:Double):Boolean {
+        return (number.toInt().toDouble() == number)
     }
 
     // check which operation must be done
@@ -105,7 +148,7 @@ class MainActivity : AppCompatActivity() {
             "-" -> return restar(n1,n2)
             "*" -> return multiplicar(n1,n2)
             "/" -> return dividir(n1,n2)
-            else -> return 0.0
+            else -> return 0.0 //never
         }
     }
 
@@ -124,5 +167,28 @@ class MainActivity : AppCompatActivity() {
 
     fun dividir(n1:Double,n2:Double):Double{
         return n1/n2
+    }
+
+    // Function to save the Instance state
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putDouble("result",result)
+        outState.putDouble("number",number)
+        outState.putString("operation",operation)
+        outState.putBoolean("clean",clean)
+        d {"rugolid onSaveInstanceState"}
+        d { "rugolid STATES: " + result.toString() + " " + number.toString() + " " + operation + " "+clean.toString()}
+    }
+
+    // Function to restore the Instance state
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        result = savedInstanceState.getDouble("result")
+        number = savedInstanceState.getDouble("number")
+        // getString espera String?, así que tengo que añadir el "toString()"
+        operation = savedInstanceState.getString("operation").toString()
+        clean = savedInstanceState.getBoolean("clean")
+        d {"rugolid onRestoreInstanceState"}
+        d { "rugolid STATES: " + result.toString() + " " + number.toString() + " " + operation + " "+clean.toString()}
     }
 }
